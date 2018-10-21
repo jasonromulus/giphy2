32f8de08e90298f2f23a5826c81d051bf6464201
jasonromulus <pumcome@me.com>
2018-10-21 00:52:06 -0700

    Fixed code stuff

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 var express = require('express');
-var app = express();
 var exphbs  = require('express-handlebars');
+var app = express();
 var http = require('http');
 var giphy = require('giphy-api')();
+var port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    giphy.search(req.query.term, function (err, response) {
+    let search_term = req.query.term || "crazy rich asians"
+    giphy.search(search_term, function (err, response) {
       res.render('home', {gifs: response.data})
     });
   });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Gif Search listening on port localhost:3000!');
 });
